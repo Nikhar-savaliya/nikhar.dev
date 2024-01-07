1595a270faeb7ea0216c9c501deceafda9387206
Nikhar S <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-01-08 05:06:00 +0530

    add : contact.tsx to layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import Contact from "./components/Contact";
 
 const Layout: React.FC = () => {
   return (
     <div className="text-zinc-950 bg-zinc-50 font-sans">
       <Header />
       <Outlet />
+      <Contact />
       <Footer />
     </div>
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,30 @@
+import { Mail } from "lucide-react";
+import Badge from "./ui/Badge";
+import { Link } from "react-router-dom";
+import Button from "./ui/Button";
+
+const Contact = () => {
+  return (
+    <section className="flex flex-col gap-2 items-center justify-center max-w-6xl mt-36 mb-24 px-4 mx-auto">
+      <Badge variant="success">Open for full-time jobs or freelance</Badge>
+
+      <p className="font-serif text-3xl md:text-4xl max-w-lg text-center">
+        Want to contact me for your next Project?
+      </p>
+      <p className="max-w-md text-xs md:text-sm py-4 text-center text-zinc-600">
+        I'm currently looking for new opportunities to grow my skills. Let's
+        chat if you are interseted!
+      </p>
+      <Link to="mailto:nikhar663@gmail.com" target="_blank">
+        <div title="Mail To: nikhar663@gmail.com">
+          <Button variant="default">
+            <Mail />
+            Get in Touch
+          </Button>
+        </div>
+      </Link>
+    </section>
+  );
+};
+
+export default Contact;
