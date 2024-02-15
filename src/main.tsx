1bd2246cc259251b36acce57c1d2d40e2dcf1e47
Nikhar-savaliya <nikhar663@gmail.com>
2024-02-15 21:43:50 +0530

    edit: router & compressed png images

--- src/main.tsx
+++ src/main.tsx
@@ -2,44 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Layout from "./Layout.tsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AboutMe from "./pages/AboutMe.tsx";
 import Projects from "./pages/Projects.tsx";
 import Articles from "./pages/Articles.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import App from "./App.tsx";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "",
-        element: <App />,
-      },
-      {
-        path: "about-me",
-        element: <AboutMe />,
-      },
-      {
-        path: "projects",
-        element: <Projects />,
-      },
-      {
-        path: "articles",
-        element: <Articles />,
-      },
-    ],
-  },
-  {
-    path: "/*",
-    element: <NotFound />,
-  },
-]);
+const AppRouter = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="about-me" element={<AboutMe />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="articles" element={<Articles />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={appRouter} />
+    <AppRouter />
   </React.StrictMode>
 );
+
+export default AppRouter;
